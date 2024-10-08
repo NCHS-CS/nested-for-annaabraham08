aa2774aaaa9ffcc2fd16fa6774cf9753030e75d8
annaabraham08 <1118485@apps.nsd.org>
2024-10-08 06:28:57 +0000

    done!

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,52 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App {
+    public static void main(String[] args) {
+        practice1();
+        practice2();
+        practice3();
+        /* 
+        for (int num = 1; num <= 5; num++) {
+            for (int counter = 1; counter <= 4; counter++) {
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+        */
+    }
 
-package org.nhs.app;
+    public static void practice1() {
+        for (int i = 1; i <= 5; i++) {
+            for (int counter = 1; counter <= i; counter++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
-// Your code goes here
+    public static void practice2() {
+        for (int i = 1; i <= 5; i++) {
+            for (int dot = 1; dot <= (5-i); dot++) {
+                System.out.print(".");
+            }
+            for (int counter = 1; counter <= i; counter++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void practice3() {
+        for (int i = 1; i <= 5; i++) {
+            for (int dot = 1; dot <= (5-i); dot++) {
+                System.out.print(".");
+            }
+            System.out.print(i);
+
+            for (int x = 5-i; x < 4 ; x++) {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
+}
